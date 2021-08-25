2a1c735fe43f11264ef2036ef7bc7646fcb217ee
Copperhead <jeremy.w.leeder@gmail.com>
2021-08-25 01:25:28 -0700

    json update

--- contracts/CSPFORCE_NFT.sol
+++ contracts/CSPFORCE_NFT.sol
@@ -108,5 +108,21 @@ contract CSPFORCE is
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+  
+  /**
+   * Override isApprovedForAll to auto-approve OS's proxy contract
+   */
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public override view returns (bool isOperator) {
+      // if OpenSea's ERC721 Proxy Address is detected, auto-return true
+        if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
+            return true;
+        }
+        
+        // otherwise, use the default ERC721.isApprovedForAll()
+        return ERC721.isApprovedForAll(_owner, _operator);
     }    
 }
