139a603c40da75b2ea119bf82507fc0f974cf968
Copperhead <jeremy.w.leeder@gmail.com>
2021-08-25 17:36:13 -0700

    formatting

--- contracts/CSFCard_NFT.sol
+++ contracts/CSFCard_NFT.sol
@@ -9,46 +9,57 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract CryptoSpaceForceCard is 
-  ERC721, 
-  Ownable, 
-  ERC721Enumerable, 
-  ERC721Pausable, 
-  AccessControlEnumerable
+contract CryptoSpaceForceCard is
+    ERC721,
+    Ownable,
+    ERC721Enumerable,
+    ERC721Pausable,
+    AccessControlEnumerable
 {
-  using Counters for Counters.Counter;
-  
-  string dynamicBaseURI;
-  mapping(uint256 => Card) public cards;
-  mapping(address => uint256[]) private _ownedTokens;
-  
-      struct Card {
+    using Counters for Counters.Counter;
+
+    string dynamicBaseURI;
+    mapping(uint256 => Card) public cards;
+    mapping(address => uint256[]) private _ownedTokens;
+
+    struct Card {
         string cardId;
         string uri;
     }
-    
-  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-  
-  Counters.Counter private _tokenIds;
-  
-  mapping (uint256 => string) private _tokenURIs;
-  
-  event LockCard(address indexed holder, string cardId, string steemAddr, uint256 indexed _TokenId);
-  event MintCard(address indexed holder, string cardId, uint256 indexed tokenId);
-  
-   /**
-    * @dev Make sure to pass trailing seperator on url 
-    * - Ex: https://gateway.pinata.cloud/ipfs/
-    */
-   constructor(string memory _baseTokenURI) ERC721("Crypto Space Force", "CSFCARD") {
-      dynamicBaseURI = _baseTokenURI;
-      
-      _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-      
-      _setupRole(MINTER_ROLE, _msgSender());
-      _setupRole(PAUSER_ROLE, _msgSender());
-   }
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    Counters.Counter private _tokenIds;
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    event LockCard(
+        address indexed holder,
+        string cardId,
+        string steemAddr,
+        uint256 indexed _TokenId
+    );
+    event MintCard(
+        address indexed holder,
+        string cardId,
+        uint256 indexed tokenId
+    );
+
+    /**
+     * @dev Make sure to pass trailing seperator on url
+     * - Ex: ipfs:// or https://ipfs.io/ipfs/ or https://gateway.pinata.cloud/ipfs/
+     */
+    constructor(string memory _baseTokenURI)
+        ERC721("Crypto Space Force", "CSFCARD")
+    {
+        dynamicBaseURI = _baseTokenURI;
+
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+    }
 
     function baseTokenURI() public view returns (string memory) {
         return dynamicBaseURI;
@@ -58,13 +69,20 @@ contract CryptoSpaceForceCard is
         dynamicBaseURI = _newBaseURI;
     }
 
-    function cardIdForTokenId(uint256 _tokenId) public view returns (string memory) {
+    function cardIdForTokenId(uint256 _tokenId)
+        public
+        view
+        returns (string memory)
+    {
         return cards[_tokenId].cardId;
     }
 
-   function burn(uint256 tokenId) public virtual {
+    function burn(uint256 tokenId) public virtual {
         //solhint-disable-next-line max-line-length
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Burnable: caller is not owner nor approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721Burnable: caller is not owner nor approved"
+        );
         _burn(tokenId);
     }
 
@@ -73,77 +91,94 @@ contract CryptoSpaceForceCard is
      * @param owner address owning the tokens
      * @return uint256[] List of token IDs owned by the requested address
      */
-    function _tokensOfOwner(address owner) internal view returns (uint256[] memory) {
+    function _tokensOfOwner(address owner)
+        internal
+        view
+        returns (uint256[] memory)
+    {
         uint256 length = ERC721.balanceOf(owner);
         require(length > 0, "ERC721TokensOfOwner: caller balance is zero");
-        
+
         uint256[] memory _tokens = new uint256[](length);
         for (uint256 i = 0; i < length; i++) {
-          _tokens[i] = _ownedTokens[owner][i];
+            _tokens[i] = _ownedTokens[owner][i];
         }
         return _tokens;
     }
-    
-    function tokensOfHolder(address holder) public view returns (uint256[] memory) {
+
+    function tokensOfHolder(address holder)
+        public
+        view
+        returns (uint256[] memory)
+    {
         return _tokensOfOwner(holder);
     }
-    
-    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
-        return strConcat(
-            baseTokenURI(),
-            cards[_tokenId].uri
-        );
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        string memory _path = strConcat(baseTokenURI(), cards[_tokenId].uri);
+        _path = strConcat(_path, "/",cards[_tokenId].cardId,".json");
+        return _path;
     }
 
-  /**
-    * @dev calculates the next token ID based on value of _currentTokenId
-    * @return uint256 for the next token ID
-    */
-  function _getNextTokenId() internal view returns (uint256) {
-    return _tokenIds.current() + 1;
-  }
-  
-  /**
-   * @return unint256 of the token ID
-   */
-  function mint(address recipient) private returns (uint256) {
-    _tokenIds.increment();
-    uint256 newItemId = _tokenIds.current();
-    _mint(recipient, newItemId);
-    return newItemId;
-  }
-  
-  /**
-   * @dev instead of the standard mint we use mint Card
-   * 
-   * Requirements:
-   * - Must supply the Card Id and URI tag to append to baseTokenURI
-   */
-  function mintCard(address _recipient, string memory _cardId, string memory _uriId) 
-    public 
-    returns (uint256) 
-  {
-    require(!paused(), "ERC721Pausable: no token minting while paused");
-    require(hasRole(MINTER_ROLE, _msgSender()), "ERC721: minter only");
-    
-    uint256 newTokenId = _getNextTokenId();
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() internal view returns (uint256) {
+        return _tokenIds.current() + 1;
+    }
 
-    cards[newTokenId].cardId = _cardId;
-    cards[newTokenId].uri = _uriId;
+    /**
+     * @return unint256 of the token ID
+     */
+    function mint(address recipient) private returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        return newItemId;
+    }
 
-    uint256 newItemId = mint(_recipient);
-    require(_getNextTokenId() == newTokenId + 1, "Mint Card: Safety Check");
+    /**
+     * @dev instead of the standard mint we use mint Card
+     *
+     * Requirements:
+     * - Must supply the Card Id and URI tag to append to baseTokenURI
+     */
+    function mintCard(
+        address _recipient,
+        string memory _cardId,
+        string memory _uriId
+    ) public returns (uint256) {
+        require(!paused(), "ERC721Pausable: no token minting while paused");
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC721: minter only");
 
-    emit MintCard(_recipient, _cardId, newTokenId);
-    return newItemId;
-  }
-  
-     /**
-      * @dev Give the card back to the contract
-      * - used in web transactions
-      */
-     function lockCard(uint256 _TokenId, string memory _steemAddr) public {
-        require(ownerOf(_TokenId) == _msgSender(), "ERC721LockCard: caller is not the owner");
+        uint256 newTokenId = _getNextTokenId();
+
+        cards[newTokenId].cardId = _cardId;
+        cards[newTokenId].uri = _uriId;
+
+        uint256 newItemId = mint(_recipient);
+        require(_getNextTokenId() == newTokenId + 1, "Mint Card: Safety Check");
+
+        emit MintCard(_recipient, _cardId, newTokenId);
+        return newItemId;
+    }
+
+    /**
+     * @dev Give the card back to the contract
+     * - used in web transactions
+     */
+    function lockCard(uint256 _TokenId, string memory _steemAddr) public {
+        require(
+            ownerOf(_TokenId) == _msgSender(),
+            "ERC721LockCard: caller is not the owner"
+        );
 
         string memory cardId = cardIdForTokenId(_TokenId);
         transferFrom(msg.sender, address(this), _TokenId);
@@ -151,18 +186,21 @@ contract CryptoSpaceForceCard is
         emit LockCard(msg.sender, cardId, _steemAddr, _TokenId);
     }
 
-    function unlockCard(uint256 _TokenId, address _newHolder) public isLockedCard(_TokenId) {
+    function unlockCard(uint256 _TokenId, address _newHolder)
+        public
+        isLockedCard(_TokenId)
+    {
         require(hasRole(MINTER_ROLE, _msgSender()), "ERC721: minter only");
-        
+
         transferFrom(address(this), _newHolder, _TokenId);
     }
- 
-     modifier isLockedCard(uint256 _TokenId) {
+
+    modifier isLockedCard(uint256 _TokenId) {
         require(ownerOf(_TokenId) == address(this), "ERC721Card: Not Locked");
         _;
     }
-    
-      /**
+
+    /**
      * @dev Pauses all token transfers.
      *
      * See {ERC721Pausable} and {Pausable-_pause}.
@@ -172,7 +210,10 @@ contract CryptoSpaceForceCard is
      * - the caller must have the `PAUSER_ROLE`.
      */
     function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have pauser role to pause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "ERC721PresetMinterPauserAutoId: must have pauser role to pause"
+        );
         _pause();
     }
 
@@ -186,7 +227,10 @@ contract CryptoSpaceForceCard is
      * - the caller must have the `PAUSER_ROLE`.
      */
     function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have pauser role to unpause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "ERC721PresetMinterPauserAutoId: must have pauser role to unpause"
+        );
         _unpause();
     }
 
@@ -210,52 +254,74 @@ contract CryptoSpaceForceCard is
     {
         return super.supportsInterface(interfaceId);
     }
-  
-  /**
-   * Override isApprovedForAll to auto-approve OS's proxy contract
-   * - for OpenSea
-   */
-    function isApprovedForAll(
-        address _owner,
-        address _operator
-    ) public override view returns (bool isOperator) {
-      // if OpenSea's ERC721 Proxy Address is detected, auto-return true
+
+    /**
+     * Override isApprovedForAll to auto-approve OS's proxy contract
+     * - for OpenSea
+     */
+    function isApprovedForAll(address _owner, address _operator)
+        public
+        view
+        override
+        returns (bool isOperator)
+    {
+        // if OpenSea's ERC721 Proxy Address is detected, auto-return true
         if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
             return true;
         }
-        
+
         // otherwise, use the default ERC721.isApprovedForAll()
         return ERC721.isApprovedForAll(_owner, _operator);
     }
-    
-   // via https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
-  function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) internal pure returns (string memory) {
-      bytes memory _ba = bytes(_a);
-      bytes memory _bb = bytes(_b);
-      bytes memory _bc = bytes(_c);
-      bytes memory _bd = bytes(_d);
-      bytes memory _be = bytes(_e);
-      string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-      bytes memory babcde = bytes(abcde);
-      uint k = 0;
-      for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-      for (uint i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-      for (uint i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-      for (uint i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-      for (uint i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-      return string(babcde);
+
+    // via https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
+    function strConcat(
+        string memory _a,
+        string memory _b,
+        string memory _c,
+        string memory _d,
+        string memory _e
+    ) internal pure returns (string memory) {
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        bytes memory _bc = bytes(_c);
+        bytes memory _bd = bytes(_d);
+        bytes memory _be = bytes(_e);
+        string memory abcde = new string(
+            _ba.length + _bb.length + _bc.length + _bd.length + _be.length
+        );
+        bytes memory babcde = bytes(abcde);
+        uint256 k = 0;
+        for (uint256 i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+        for (uint256 i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+        for (uint256 i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
+        for (uint256 i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
+        for (uint256 i = 0; i < _be.length; i++) babcde[k++] = _be[i];
+        return string(babcde);
     }
 
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) internal pure returns (string memory) {
+    function strConcat(
+        string memory _a,
+        string memory _b,
+        string memory _c,
+        string memory _d
+    ) internal pure returns (string memory) {
         return strConcat(_a, _b, _c, _d, "");
     }
 
-    function strConcat(string memory _a, string memory _b, string memory _c) internal pure returns (string memory) {
+    function strConcat(
+        string memory _a,
+        string memory _b,
+        string memory _c
+    ) internal pure returns (string memory) {
         return strConcat(_a, _b, _c, "", "");
     }
 
-    function strConcat(string memory _a, string memory _b) internal pure returns (string memory) {
+    function strConcat(string memory _a, string memory _b)
+        internal
+        pure
+        returns (string memory)
+    {
         return strConcat(_a, _b, "", "", "");
     }
-    
 }
