496c66580006a7b265d14b02e489521b8cb694f4
Copperhead <jeremy.w.leeder@gmail.com>
2022-05-19 14:10:40 -0700

    presale

--- contracts/presale.sol
+++ contracts/presale.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Presale is ReentrancyGuard {
+contract PRESALECSOLS is ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     
@@ -104,4 +104,5 @@ contract Presale is ReentrancyGuard {
         uint256 amount = token.balanceOf(address(this));
         token.safeTransfer(msg.sender, amount);
     }
+    
 }
